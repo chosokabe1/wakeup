40103b6d4ae3c4e2fdaf472d9c4bad4cdc9ba888
chosokabe1 <tktkshi101110@gmail.com>
2022-07-21 05:03:30 +0900

    videocap

---
+++ videocapture.py
@@ -0,0 +1,15 @@
+import cv2
+
+cap = cv2.VideoCapture(1)
+while True:
+  ret, frame = cap.read()
+
+  cv2.imshow("Frame", frame)
+  key = cv2.waitKey(1)
+  if key == 27:
+    break   
+    
+cap.release()
+cv2.destroyAllWindows()
+
+cv2.imwrite('sample.jpg', frame)
